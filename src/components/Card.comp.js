0ba28949def5a462eb38b74bf46aaa92191ef2c3
Aaron <info@aarontan.co.nz>
2023-01-21 14:24:49 +1300

    add remote repo

--- src/components/Card.comp.js
+++ src/components/Card.comp.js
@@ -1,13 +1,19 @@
 import classes from './Card.module.css'
 import { useState } from 'react'
+import { FiChevronsLeft, FiChevronsRight } from 'react-icons/fi'
 
 function Card(props) {
   const [imageDisplay, setImageDisplay] = useState()
+
+  const imageHandleClick = () => {
+    setImageDisplay()
+  }
 
   console.log('CARD COMPONENT', props.image)
   return (
     <div>
       <img className={classes.image} src={props.image[2]} />
+      <FiChevronsRight onClick={imageHandleClick} />
     </div>
   )
 }
